fa7ba72a1ad6dd54dd864216518aa9abf8e43871
Rihan Dhakal <rihandhakal10@gmail.com>
2025-12-14 17:33:10 +0545

    added js

--- script.js
+++ script.js
@@ -6,3 +6,51 @@
         prevEl: "#prev",
       },
     });
+
+    const carticon= document.querySelector('.cart-icon');
+    const carttab=document.querySelector('.cart-tab');
+    const closebtn=document.querySelector('.close-btn');
+    const cardList=document.querySelector('.card-list');
+
+    carticon.addEventListener('click',()=>carttab.classList.add('cart-tab-active'));
+    closebtn.addEventListener('click',()=> carttab.classList.remove('cart-tab-active'));
+
+    let productList=[]
+
+    const showCarts=()=>{
+
+      productList.forEach(product=>{
+        const orderCard = document.createElement('div')
+        orderCard.classList.add('order-card')
+        orderCard.innerHTML=`
+        
+          <div class="card-image">
+            <img src="${product.image}" >
+          </div>
+          <h4>${product.name}</h4>
+          <h4 class="price">${product.price}</h4>
+          <a href="#" class="btn add-to-cart-btn">Add to cart</a>
+        `
+
+        cardList.appendChild(orderCard)
+        
+      })
+
+
+    }
+
+
+
+    const initApp=()=>{
+
+      fetch('products.json').then
+      (response=>response.json()).then
+      (data=>{
+        productList=data;
+        showCarts()
+      })
+
+
+    }
+
+    initApp();
